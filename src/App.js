25f31b1a143f808a99a5638e987f7a926f821f72
Ale007XD <alex.deloverov@gmail.com>
2025-07-29 10:53:40 +0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import Navbar from './components/Navbar';
+import Auth from './components/Auth';
+import Chat from './components/Chat';
+import History from './components/History';
+import authService from './services/authService';
+
+export default class App {
+    constructor() {
+        this.currentUser = null;
+        this.currentView = 'auth';
+        this.navbar = new Navbar(this.handleNavigation.bind(this), this.currentUser);
+        this.initializeApp();
+    }
+
+    initializeApp() {
+        document.body.appendChild(this.navbar.element);
+        this.updateView();
+        
+        // Следим за изменением состояния аутентификации
+        authService.onAuthStateChanged((user) => {
+            this.currentUser = user;
+            this.navbar.update(user);
+            this.updateView();
+        });
+    }
+
+    updateView() {
+        const appContainer = document.getElementById('app');
+        appContainer.innerHTML = '';
+
+        if (!this.currentUser) {
+            this.currentView = 'auth';
+            const auth = new Auth(() => {
+                // После успешной авторизации обновим представление
+                setTimeout(() => this.updateView(), 100);
+            });
+            appContainer.appendChild(auth.element);
+        } else {
+            switch (this.currentView) {
+                case 'history':
+                    const history = new History(this.handleSelectChat.bind(this));
+                    appContainer.appendChild(history.element);
+                    break;
+                case 'chat':
+                default:
+                    const chat = new Chat();
+                    appContainer.appendChild(chat.element);
+                    break;
+            }
+        }
+    }
+
+    handleNavigation(view) {
+        this.currentView = view;
+        this.updateView();
+    }
+
+    handleSelectChat(chatItem) {
+        this.currentView = 'chat';
+        this.updateView();
+        // Здесь можно добавить логику для отображения выбранного чата
+    }
+}
